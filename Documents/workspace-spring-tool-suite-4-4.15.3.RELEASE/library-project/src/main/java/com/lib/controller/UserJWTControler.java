e0a3d01cab2bbbdbadd86db5a1612566a284a71f
Emrullah Celebi <cumali20182019@gmail.com>
2022-10-22 08:08:44 +0200

    22.10.2022

--- Documents/workspace-spring-tool-suite-4-4.15.3.RELEASE/library-project/src/main/java/com/lib/controller/UserJWTControler.java
+++ Documents/workspace-spring-tool-suite-4-4.15.3.RELEASE/library-project/src/main/java/com/lib/controller/UserJWTControler.java
@@ -2,26 +2,30 @@ package com.lib.controller;
 
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.lib.security.JwtUtils;
 import com.lib.service.UserService;
 import com.lib.controller.dto.LoginRequest;
 import com.lib.controller.dto.RegisterRequest;
+import com.lib.controller.dto.UpdateRequestDTO;
+import com.lib.domain.User;
 
 import lombok.AllArgsConstructor;
 
@@ -32,7 +36,7 @@ public class UserJWTControler {
 
 	
 	@Autowired
-	private UserService userservice;
+	private UserService userService;
 	
 	@Autowired
 	AuthenticationManager authenticationManager;
@@ -44,7 +48,7 @@ public class UserJWTControler {
 		@PostMapping("/register")
 		//DTO lari olusturup kullanmamizin sebebi User de id felan var onlari almamak icin
 		public ResponseEntity<Map<String,String>> registerUser(@Valid @RequestBody RegisterRequest request){
-			userservice.registerUser(request);
+			userService.registerUser(request);
 			
 			Map<String,String> map=new HashMap<>();
 			map.put("message", "User registered successfuly");
@@ -71,5 +75,38 @@ public class UserJWTControler {
 				return new ResponseEntity<>(map,HttpStatus.ACCEPTED);
 				
 			}
+			//tum  users lari getiriyoruz
+			@GetMapping("/listUsers")
+		    @PreAuthorize("hasRole('ROLE_ADMIN')")//sadece admin olan kişi getAll yapabilsin
+		    public ResponseEntity<List<User>> getAll() {
+		        // @RequestBody = gelen parametreyi, requestin bodysindeki bilgisi , 
+		           //Student objesine map edilmesini sağlıyor.
+		        List<User> users=  userService.getAll();
+		        return ResponseEntity.ok(users);
+		    }
 	
+			// Delete User
+			@DeleteMapping("/delete/{id}")
+			@PreAuthorize("hasRole('ROLE_ADMIN')")//sadece admin olan kişi delete yapabilsin
+			public ResponseEntity<Map<String,String>> deleteUser(@PathVariable("id") Long id) {
+				userService.deleteUser(id);
+				Map<String,String> map = new HashMap<>();
+				map.put("message", "User is deleted successfuly");
+				map.put("status", "true");
+				return new ResponseEntity<>(map,HttpStatus.OK);
+				//todo kullanicinin iade etmedigi kitabi varsa silmesin
+				
+			}
+			
+			//update Student, DTO kullanarak yapacağız
+			@PutMapping("/update/{id}")//.../students/1
+			@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+			public ResponseEntity<Map<String, String>> updateUser( @PathVariable Long id, @RequestBody UpdateRequestDTO updateRequestDTO){
+				
+				userService.updateUser(id,updateRequestDTO);
+				Map<String,String> map = new HashMap<>();
+				map.put("message", "User is updated successfuly");
+				map.put("status", "true");
+				return new ResponseEntity<>(map,HttpStatus.OK);
+			}
 }
--- Documents/workspace-spring-tool-suite-4-4.15.3.RELEASE/library-project/src/main/java/com/lib/service/UserService.java
+++ Documents/workspace-spring-tool-suite-4-4.15.3.RELEASE/library-project/src/main/java/com/lib/service/UserService.java
@@ -1,15 +1,15 @@
 package com.lib.service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import com.lib.repository.RoleRepository;
 import com.lib.repository.UserRepository;
 import com.lib.controller.dto.RegisterRequest;
+import com.lib.controller.dto.UpdateRequestDTO;
 import com.lib.domain.Role;
 import com.lib.domain.User;
 import com.lib.domain.enums.UserRole;
@@ -55,4 +55,48 @@ public class UserService {
 		
 		userRepository.save(user);
 	}
+
+
+	public List<User> getAll() {
+		
+		return userRepository.findAll();
+	}
+
+
+	public void deleteUser(Long id) {
+        User user = findUser(id);
+        userRepository.delete(user);
+
+   }
+
+
+	//find User By Id
+		public User findUser(Long id) {
+			return userRepository.findById(id).
+			orElseThrow(()->new ResourceNotFoundException("User not found with id: "+id));
+		}
+
+
+		//Update User
+		public void updateUser(Long id, UpdateRequestDTO updateRequestDTO) {
+			boolean emailExist= userRepository.existsByUserMail(updateRequestDTO.getUserMail());
+			
+			
+			User user= findUser(id);
+			
+			if(emailExist && !updateRequestDTO.getUserMail().equals(user.getUserMail())) {
+				throw new ConflictException("Email is already exist");
+			}
+			
+			//yeni girisleri pojo classimiza set ediyoruz
+			user.setFirstName(updateRequestDTO.getFirstName());
+			user.setLastName(updateRequestDTO.getLastName());
+			user.setPassword(updateRequestDTO.getPassword());
+			user.setUserMail(updateRequestDTO.getUserMail());
+			user.setPhoneNumber(updateRequestDTO.getPhoneNumber());
+			
+			
+			//save islemi
+			userRepository.save(user);
+		}
 }
